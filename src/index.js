00c690a4087784565d2a703113cbe78de8e5c947
Kotinann <Kotina1601@gmail.com>
2022-09-24 17:10:17 +0400

    mobile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import helloWorld from "./scripts/helloWorld";
 import Prism from "prismjs";
+import helloWorld from "./scripts/helloWorld";
+import Prism from "prismjs";
 
 import "prismjs/themes/prism-coy.css";
 import "prismjs/components/prism-scss";
-
+/*= ============== PASSWORD =============== */
+$('body').on('click', '.menu__password-control', function(){
+    if ($('#password-input').attr('type') == 'password'){
+        $(this).addClass('view');
+        $('#password-input').attr('type', 'text');
+    } else {
+        $(this).removeClass('view');
+        $('#password-input').attr('type', 'password');
+    }
+    return false;
+});
